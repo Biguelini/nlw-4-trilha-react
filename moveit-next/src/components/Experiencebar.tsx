a255f5134d0be76a5d17be2507479c32b890a382
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-02-25 21:05:56 -0300

    dia 3

--- moveit-next/src/components/Experiencebar.tsx
+++ moveit-next/src/components/Experiencebar.tsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function Experiencebar(){
+    const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
+
+    const percentToNextLevel = Math.round((currentExperience*100)/experienceToNextLevel)
+
     return(
         <header className={styles.experienceBar}>
             <span>0Xp</span>
                 <div>
-                    <div style={{ width: '50%' }}/>
-                    <span className={styles.currentExperience} style={{ left: '50%' }}>
-                        300Xp
+                    <div style={{ width: `${percentToNextLevel}%;` }}/>
+                    <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%;` }}>
+                        {currentExperience}Xp
                     </span>
                 </div>
-            <span>600Xp</span>
+            <span>{experienceToNextLevel}Xp</span>
         </header>
     );
 }
--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,12 +1,21 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json'
 
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
+    experienceToNextLevel: number;
 }
 
 
@@ -22,12 +31,25 @@ export function ChallengesProvider({children}:ChallengesProviderProps){
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level+1)*4,2)
+
+
     function levelUp(){
         setLevel(level+1);
     }
     function startNewChallenge(){
-        console.log('New challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+        setActiveChallenge(challenge);
     }
+
+    function resetChallenge(){
+        setActiveChallenge(null);
+    }
+
     return(
         <ChallengesContext.Provider 
         value={{
@@ -35,7 +57,10 @@ export function ChallengesProvider({children}:ChallengesProviderProps){
             currentExperience, 
             challengesCompleted, 
             levelUp,
-            startNewChallenge
+            startNewChallenge,
+            activeChallenge,
+            resetChallenge,
+            experienceToNextLevel
         }}>
             {children}
         </ChallengesContext.Provider>
